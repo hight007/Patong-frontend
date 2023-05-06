210c8680f0c50a3040da86f600beeb3add4e1084
hight007 <hight_007@hotmail.com>
2023-05-06 14:30:28 +0700

    apiUrl 1.3.0

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,8 +23,8 @@ export const NO = "NO";
 export const OK = "ok";
 export const NOK = "nok";
 
-export const apiUrl = "http://localhost:2008/api/patong/";
-// export const apiUrl = "https://asia-east1-patong-379114.cloudfunctions.net/Patong_backend/api/patong/";
+// export const apiUrl = "http://localhost:2008/api/patong/";
+export const apiUrl = "https://asia-east1-patong-379114.cloudfunctions.net/Patong_backend/api/patong/";
 
 export const apiName = {
   authen: {
